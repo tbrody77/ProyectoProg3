43ccd2fcf0bd9ece7826243f1778ca7e84de5251
arielbercovich <abercovich@udesa.edu.ar>
2023-09-04 10:52:32 -0300

    flf

---
+++ src/Components/Title/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from '../../img/logo.png'
+import brober from '../../img/brober.png'
+
+function Title(){
+    return(<img src={logo} alt="" />
+    )
+}
+
+export default Title 
